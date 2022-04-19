75a5e2ad1272333a397a1eed42adf6a411224a12
Rogenis Silva <rps@icomp.ufam.edu.br>
2022-04-18 21:56:59 -0400

    Reformulando estrutura jsx

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Olá Rógeniss</Text>
+    </View>
+  );
+}
