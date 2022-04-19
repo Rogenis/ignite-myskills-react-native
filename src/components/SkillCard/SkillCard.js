40b186e8f02be6725f626329b7da4e322527d21f
Rogenis Silva <rps@icomp.ufam.edu.br>
2022-04-19 16:53:56 -0400

    add component skillCard

---
+++ src/components/SkillCard/SkillCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import {styles} from './styles';
+
+export default function SkillCard({skill}) {
+  return (
+    <TouchableOpacity key={skill} style={styles.buttonSkill}>
+      <Text style={styles.textSkill}>{skill}</Text>
+    </TouchableOpacity>
+  );
+}
