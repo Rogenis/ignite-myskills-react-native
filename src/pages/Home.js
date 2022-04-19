29eaa3b4fae3ca2fe44d8f342dfc9574d50785ac
Rogenis Silva <rps@icomp.ufam.edu.br>
2022-04-18 23:31:05 -0400

    Adiciona uso de estados na Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 import {styles} from './styles';
 
 export default function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills([...mySkills, newSkill]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome Rógenis!</Text>
@@ -10,13 +17,23 @@ export default function Home() {
         style={styles.input}
         placeholder="New Skill"
         placeholderTextColor="#555"
+        onChangeText={setNewSkill}
       />
 
-      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={handleAddNewSkill}>
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
-      <Text style={[styles.title, {marginTop: 50}]}>My Skills</Text>
+      <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+
+      {mySkills.map(skill => (
+        <TouchableOpacity key={skill} style={styles.buttonSkill}>
+          <Text style={styles.textSkill}>{skill}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
